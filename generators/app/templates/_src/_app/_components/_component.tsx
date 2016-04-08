3f78eba6c3d6cf66ddb75eb2b6e0e23de7a40a7d
lobsteropteryx <ian.firkin@gmail.com>
2016-04-07 23:27:40 -0400

    Imports interfaces in TSX template

--- generators/app/templates/_src/_app/_components/_component.tsx
+++ generators/app/templates/_src/_app/_components/_component.tsx
@@ -1,7 +1,7 @@
 /// <reference path="../../../typings/tsd.d.ts" />
 
 import * as React from 'react';
-import <%=capModel%> from '../models/<%=capModel%>';
+import {<%=capModel%>, I<%=capModel%>Props,I<%=capModel%>State} from '../models/<%=capModel%>';
 
 export default class <%=capName%> extends React.Component<I<%=capName%>Props, I<%=capName%>State> {
     constructor(props:I<%=capName%>Props) {

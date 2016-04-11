9a9144849d80c59275b77760ecf4559e5cd92249
Ian Firkin <ian.firkin@gisinc.com>
2016-04-11 12:54:52 -0400

    Removes default export from model template

--- generators/app/templates/_src/_app/_models/_model.ts
+++ generators/app/templates/_src/_app/_models/_model.ts
@@ -6,7 +6,7 @@ export interface I<%=capName%>State {
 
 }
 
-export default class <%=capModel%> implements I<%=capName%>Props {
+export class <%=capModel%> implements I<%=capName%>Props {
     constructor(public someProperty: string) {
         Object.freeze(this); // Raise an exception if we mutate
     }
